37ef1b00421f13b4d6758841ad05e89204011575
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-30 21:23:26 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Edwin Przeźwiecki Jr. on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
